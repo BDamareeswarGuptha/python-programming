5e969384e3350b00772ab0dc479e3c39ff0c3433
BDamareeswarGuptha <sskdamareesb@gmail.com>
2025-09-02 12:15:47 +0530

    Add files via upload

---
+++ Running Python Script and various expressions in an interactive interpreter 1b.py
@@ -0,0 +1,21 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================================ RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python312/Running Python Script and various expressions in an interactive interpreter.py ============================================
+Enter the First number:11
+Enter the Second number:21
+Addition: 32
+Subtraction : -10
+Multiplication: 231
+Division: 0.5238095238095238
+>>> 
+============================================ RESTART: C:/Users/Admin/AppData/Local/Programs/Python/Python312/Running Python Script and various expressions in an interactive interpreter.py ============================================
+Enter the First number: 11
+Enter the Second number: 21
+Enter the Third number: 7
+11 > 21 is False
+11 < 21 is True
+7 == 11 is False
+7 != 21 is True
+11 >= 21 is False
+21 <= 11 is False
